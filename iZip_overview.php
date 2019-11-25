2f5eeb17491c8946c33c8b0d767b6eecbe74a715
Cydia <admin@onenightfriend.com>
2019-11-25 15:04:50 -0500

    iZip_overview.php
    YouTube

---
+++ iZip_overview.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>
+	Comc Soft Corporation, ComcSoft.com | iPhone, iPad, Android, Mobile Website Development and Outsourcing Services. Specialized in Business and Enterprise iPhone Applications Development, iPad Applications Development, Android Applications Development, Mobile Website Development, and Backend Service Integration.
+</title>
+<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
+<meta content="text/html; charset=UTF-8" http-equiv="Content-Type"/>
+<meta content="text/javascript" http-equiv="Content-Script-Type" />
+<meta content="text/css" http-equiv="Content-Style-Type" />
+<meta name="DESCRIPTION" content="Comcsoft specializes in enterprise mobile solutions and business data analytics development and out sourcing services. Comcsoft helps its clients leverage mobile and data analytics development and outsourcing services to improve their business platform so that they can compete more effectively and achieve higher ROI." />
+<meta name="KEYWORDS" content="mobile application development, enterprise mobility, SAP ERP mobile, big data analytics, hadoop, iPhone, iPad, Android, Mobile Website, ios development, outsourcing services, DFW, Dallas TX" />
+<meta name="COPYRIGHT" content="Copyright 2011 by ComcSoft " />
+<meta  name="AUTHOR" content="ComcSoft " />
+<meta  name="ROBOTS" content="INDEX, FOLLOW" />
+<meta name="REVISIT-AFTER" content="1 DAYS" />
+<meta name="RATING" content="GENERAL" />
+
+<link rel="shortcut icon" href="../../../favicon.ico">
+<link rel="stylesheet" type="text/css" href="../../../css/style.css">
+<link rel="stylesheet" type="text/css" href="../../../css/services.css">
+
+<script type="text/javascript" src="../../../js/jquery-1.3.2.min.js"></script>
+<script type="text/javascript">
+$(function(){
+	var tab_menu_li = $('.tab_menu li');
+	$('.tab_box div:gt(0)').hide();
+	
+	tab_menu_li.click(function(){
+		$(this).addClass('selected')
+			   .siblings().removeClass('selected');
+	
+	var tab_menu_li_index = tab_menu_li.index(this);
+	$('.tab_box div').eq(tab_menu_li_index).show()
+							.siblings().hide();
+	}).hover(function(){
+		$(this).addClass('hover');
+	},function(){
+		$(this).removeClass('hover');	
+	});
+});
+</script>
+<base target="_parent" />
+</head>
+
+<body>  
+<div id="top_bg">
+<div id="header">
+	<div id="header_top">
+      <div class="logo_left"><a href="../../../index.php"><img src="../../../images/logo.jpg" /></a></div>
+		<div class="tab">
+        <div id="tab_menu_w">
+          <div class="tab_menu">
+              <ul>
+                 <li><a href="../../../index.php">Home</a></li>
+                 <li><a href="../../../services.php">Services</a>
+                        <dl>
+                        <dd><a href="../../../services.php">Mobile Application Development</a></dd>
+                  	    <dd><a href="../../../services/services_Data_Analytics.php">Business Data Analytics</a></dd>
+                        </dl>
+                 </li>
+                 <li class="selected"><a href="../../../Portfolio.php">Portfolio</a>
+                       <dl>
+                            <dd><a href="../../../Portfolio.php">Mobile Applications</a></dd>
+                            <dd><a href="../../Data_Analytics.php">Big Data Analytics</a></dd>
+                       </dl>
+                 </li>
+                 <li><a href="../../../about/corporate_overview.php">About Us</a>
+                        <dl>
+                        <dd><a href="../../../about/corporate_overview.php">Corporate Overview</a></dd>
+                        <dd><a href="../../../about/values.php">Mission and Values</a></dd>
+                        <dd><a href="../../../about/locations.php">Locations</a></dd>
+                        <!-- <dd><a href="../about/work.php">Work at ComcSoft</a></dd> -->
+                        </dl>
+                    
+                 </li>
+                 <li style="border:0px; border-bottom:1px solid #ffffff;"><a href="../../../contactus/request_information.php">Contact Us</a>
+                        <dl>
+                        <dd><a href="../../../contactus/request_information.php">Request Information</a></dd>
+                        <dd><a href="../../../contactus/request_for_proposal.php">Request For Proposal</a></dd>
+                        <dd><a href="../../../contactus/sign_up_newsletters.php">Sign Up Newsletters</a></dd>
+                        </dl>    
+                 </li>
+              </ul>
+          </div>
+         </div>
+          <div class="clear"></div>  
+	</div>
+</div>
+</div>
+</div>
+
+
+<div id="banner_portfolios">
+<div id="banner_portfolios_text">
+	<div class="banner_text"></div>
+</div>
+</div>
+
+<div id="wrapMain">
+    <!-- left start  -->
+    <div id="wrapMain_left">
+		<div class="service_tab">
+			  <div class="service_tab_menu">
+               <span style=" display:block;color:#8c949a; font-size:16px; font-family:Arial, Helvetica, sans-serif; font-weight:bold; padding-bottom:5px;">Our Apps</span>
+				<ul>
+					<li class="selected"><a href="iZip_overview.php" target="_parent">iZip</a></li>
+					<li><a href="../iCatalog/iCatalog_overview.php" target="_parent">iCatalog</a></li>
+                    <li><a href="../iTransfer/iTransfer_overview.php" target="_parent">iTransfer</a></li>
+                    <li><a href="../iSafeBox/iSafeBox_overview.php" target="_parent">iSafeBox</a></li>
+                    <li><a href="../iNetTools/iNetTools_overview.php" target="_parent">iNetTools</a></li>
+                    <li><a href="../iEditor/iEditor_overview.php" target="_parent">iEditor</a></li>
+                    <li><a href="../iConverter/iConverter_overview.php" target="_parent">iConverter</a></li>
+                    <li><a href="../iTerminal/iTerminal_overview.php" target="_parent">iTerminal</a></li>
+                    <li><a href="../PowerPhoto/PowerPhoto_overview.php" target="_parent">PowerPhoto</a></li>
+                    <li><a href="../PowerPDF/PowerPDF_overview.php" target="_parent">PowerPDF</a></li>
+                    <li><a href="../PowerReader/PowerReader_overview.php" target="_parent">PowerReader</a></li>
+                    <li><a href="../PowerPlayer/PowerPlayer_overview.php" target="_parent">PowerPlayer</a></li>
+                    <li><a href="../ContactManager/ContactManager_overview.php" target="_parent">Contact Manager</a></li>
+				</ul>
+                <br/>
+                <span style=" display:block;color:#8c949a; font-size:16px; font-family:Arial, Helvetica, sans-serif; font-weight:bold; padding-bottom:5px;">
+Outsource Projects</span>
+                <ul>
+					<li><a href="../../outsource_projects/iMarket.php" target="_parent">iMarket</a></li>
+					<li><a href="../../outsource_projects/Wowpons.php" target="_parent">Wowpons</a></li>
+                    <li><a href="../../outsource_projects/Savsmylife.php" target="_parent">Savsmylife</a></li>
+                    <li><a href="../../outsource_projects/Dicota.php" target="_parent">Dicota</a></li>
+                    <li><a href="../../outsource_projects/Crossroads.php" target="_parent">Crossroads</a></li>
+			    </ul>
+			  </div>
+		</div>
+		<div><img src="../../../images/gif2.gif" width="215" height="219" /></div>
+
+    </div>
+    <!-- left end  -->
+    
+    <!-- mainR start  -->
+    <div id="wrapMain_right">
+    	<div id="mainR_padding">
+            <div class="mainR_title"></div>
+            <div class="mainR_title"></div>
+        	<div class="mainR_title">iZip</div>
+            <div class="mainR_title"><a href="iZip_overview.php" id="mainR_titlea">Overview</a>   <a href="iZip_Versions_Pricing.php">Versions/Pricing</a>   <a href="iZip_faq.php">FAQ</a></div>
+            <div class="mainR_pic"><div class="mainR_app"><img src="../../../images/izip_p.jpg" /></div><div class="mainR_app"><br /><br /><br />Available on:<br /><br />
+               <a href="http://itunes.apple.com/us/app/izip-zip-unzip-unrar-tool/id413971331?mt=8" target="_blank"><img src="../../../images/available-on-iphone.jpeg" /></a>
+               <br/><br />
+               <a href="http://play.google.com/store/apps/details?id=com.comcsoft.izip" target="_blank"><img src="../../../images/Android-app-store.png" /></a>                 
+                </div>
+            </div>
+            <div class="mainR_con"> 
+        <br/>
+            <p>No.1 App for ZIP file management on iPhone/iPad! Most secure and fast Zip utility for business and professional users.</p>
+            <p>iZip is a ZIP file management tool on iPhone and iPad. iZip has the following features: </p>
+            <p>
+           	  <ul>
+               	<li>Compress files into ZIP file. Support No Password, Plain Password, and AES Password compression.</li> 
+                <li>Compress photos and videos from Camera Roll.</li>  
+                <li>Compress songs from iTunes Music. </li> 
+                <li>Open and extract files from ZIP format, including password protected and AES encrypted ZIP files.</li>  
+                <li>Open and extract many other compressed file formats: RAR, 7Z, ZIPX, TAR, GZIP. </li> 
+                <li>Open and view many document types: DOC, Excel, PPT, PDF, TXT, RTF, Pages, JPG, GIF, PNG, videos, etc.. </li> 
+                <li>Append files to a ZIP file. </li> 
+                <li>Send files in Email. </li> 
+                <li>Open a file in another App. </li> 
+                <li>Zip/Email photos and videos from Photo Album. </li> 
+                <li>Share your photos on Facebook/Twitter/Weibo. </li> 
+                <li>Save images to Photo Album. </li> 
+                <li>Save to iCloud Drive and download from iCloud Drive.
+                <li>Upload and download files to/from Dropbox, Box, and GoogleDrive, OneDrive. </li> 
+                <li> Print documents. </li> 
+                <li> Download/upload files through iTunes. </li> 
+              </ul>
+            </p>
+            <p>With its simple interfaces, iZip provides the easiest way to manage ZIP files on iPhone and iPad. </p>
+            <br/>
+          </div>
+            <div class="mainR_title">Videos</div>
+            <div class="mainR_con"> 
+            <p style="font-size:14px; font-weight:bold;">iPhone</p> 
+            <iframe width="640" height="480" src="http://www.youtube.com/embed/7DCvW4vR-0s" frameborder="0" allowfullscreen></iframe>
+            </br></br>
+            <p style="font-size:14px; font-weight:bold;">iPad</p> 
+			<iframe width="640" height="480" src="http://www.youtube.com/embed/-iCG6_hxFx0" frameborder="0" allowfullscreen></iframe>
+    
+            </div>
+            
+      </div>
+    </div>
+    <!-- mainR end  -->
+</div>
+
+
+<script type="text/javascript">
+	
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-23422480-1']);
+	_gaq.push(['_trackPageview']);
+	
+	(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+	
+	</script>
+<div id="bottom">
+<div id="footer_link">
+         <ul>
+                 <li><img src="../../../images/b.jpg" />&nbsp;&nbsp; <a href="http://www.comcsoft.com/wordpress/">Blog</a></li>
+                 <li><img src="../../../images/f.jpg" />&nbsp;&nbsp; <a href="http://www.facebook.com/comcsoft">Be a fan on Facebook</a></li>
+                 <li><img src="../../../images/in.jpg" />&nbsp;&nbsp; <a href="http://www.linkedin.com/company/comc-soft-corporation">Find us on Linkedin</a></li>
+                 <li id="footer_link_margin"><img src="../../../images/t.jpg" />&nbsp;&nbsp; <a href="http://twitter.com/comcsoft">Follow us on Twitter</a></li>
+         </ul>
+    </div>
+<div id="copyRight">
+    <div>
+        <span id="copyRight_span01">Copyright © 2014 Comc Soft Corporation </span>
+        <span id="copyRight_span02"><a href="../../../site/sitemap.php">Sitemap</a> | <a href="../../../site/privacy.php">Privacy Statement</a></span>	 
+    </div>
+</div>
+</div>
+</body>
+</html>
